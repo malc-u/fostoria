eeec3edf72dad17e5a63e585bdde21f63c9eb2f5
MalC-U <mwcalow@gmail.com>
2020-07-07 14:35:17 +0100

    Added urls required by 'allauth'

--- fostoria/urls.py
+++ fostoria/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # urls from 'allauth' - https://django-allauth.readthedocs.io/en/latest/installation.html#post-installation
+    path('accounts/', include('allauth.urls')),
 ]
