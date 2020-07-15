0aabcda4af8e4d92a35299d7b78188fae5b69f29
MalC-U <mwcalow@gmail.com>
2020-07-15 13:00:49 +0100

    Created Group and Product models

--- products/models.py
+++ products/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+class Group(models.Model):
+    """"Model for groups that products will be assigned to"""
+    name = models.CharField(max_lenght=150)
+    display_name = models.CharField(max_length=150)
+
+    def __str__(self):
+    return self.name
+
+    def show_display_name(self):
+        return self.display_name
+
+class Product(models.Model):
+    """ Model for product instances """
+
+    title = models.CharField(max_length=254)
+    place = models.CharField(max_length=254)
+    product_image = models.ImageField(blank=False)
+    product_group = models.ForeignKey('Group', blank=False, null=False, on_delete=models.PROTECT)
+
+    def __str__(self):
+    return self.title
+
