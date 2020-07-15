403f03dbcbe3515d9e571a1a978ed54d81feae8c
MalC-U <mwcalow@gmail.com>
2020-07-15 14:03:30 +0100

    Updated model name from Group to ProductGroup

--- products/models.py
+++ products/models.py
@@ -1,15 +1,15 @@
 from django.db import models
 
 # Create your models here.
-class Group(models.Model):
+class ProductGroup(models.Model):
     """"Model for groups that products will be assigned to"""
-    name = models.CharField(max_lenght=150)
-    display_name = models.CharField(max_length=150)
+    name = models.CharField(max_length=254)
+    display_name = models.CharField(max_length=254)
 
     def __str__(self):
-    return self.name
+         return self.name
 
-    def show_display_name(self):
+    def get_display_name(self):
         return self.display_name
 
 class Product(models.Model):
@@ -18,8 +18,8 @@ class Product(models.Model):
     title = models.CharField(max_length=254)
     place = models.CharField(max_length=254)
     product_image = models.ImageField(blank=False)
-    product_group = models.ForeignKey('Group', blank=False, null=False, on_delete=models.PROTECT)
+    product_group = models.ForeignKey('ProductGroup', blank=False, null=False, on_delete=models.PROTECT)
 
     def __str__(self):
-    return self.title
+        return self.title
 
