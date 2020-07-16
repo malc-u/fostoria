0fbaaf319834a09e29b250705120add213f4fc39
MalC-U <mwcalow@gmail.com>
2020-07-16 17:13:29 +0100

    Added missing doc string to Products class

--- products/views.py
+++ products/views.py
@@ -4,6 +4,7 @@ from django.views.generic import ListView
 from .models import Product
 
 class Products(ListView):
+    """"Class that will be used as_view() and will display all products/photos"""
     template_name = "photos.html"
     context_object_name = "products"
     queryset = Product.objects.order_by('-title')
