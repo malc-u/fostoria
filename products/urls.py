449b07096d5f09537d3826f712b7c7f04f88462a
MalC-U <mwcalow@gmail.com>
2020-07-16 18:45:11 +0100

    Added url that will display each individual product details page

--- products/urls.py
+++ products/urls.py
@@ -1,8 +1,9 @@
 """Products app urls to be imported to main project urls"""
 
 from django.urls import path
-from products.views import Products
+from products.views import Products, ProductDetails
 
 urlpatterns = [
     path('', Products.as_view(), name='all_products'),
+     path('photo/<int:pk>/', ProductDetails.as_view(), name="product-detail"),
 ]
