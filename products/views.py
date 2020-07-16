fcd1f6072e9b0e178e2fa32295e3d2ba315f56c9
MalC-U <mwcalow@gmail.com>
2020-07-16 11:26:54 +0100

    Created simple view function for all_products

--- products/views.py
+++ products/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
+def all_products(request):
+    """ A view to show all products"""
+    products = Product.objects.all()
+    
+    context = {'products': products}
+    return render(request, 'photos.html', context)
