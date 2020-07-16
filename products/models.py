9b5c9d341a56dc8654bf60df314b2a511be121eb
MalC-U <mwcalow@gmail.com>
2020-07-16 17:15:31 +0100

    Added missing docstrings and removed errors highlighted by django-pylint

--- products/models.py
+++ products/models.py
@@ -1,15 +1,18 @@
+"""Products app models"""
+
 from django.db import models
 
-# Create your models here.
 class ProductGroup(models.Model):
     """"Model for groups that products will be assigned to"""
     name = models.CharField(max_length=254)
     display_name = models.CharField(max_length=254)
 
     def __str__(self):
-         return self.name
+        """Str method returning name"""
+        return self.name
 
     def get_display_name(self):
+        """Model method returning display name """
         return self.display_name
 
 class Product(models.Model):
@@ -18,8 +21,8 @@ class Product(models.Model):
     title = models.CharField(max_length=254)
     place = models.CharField(max_length=254)
     product_image = models.ImageField(blank=False)
-    product_group = models.ForeignKey('ProductGroup', blank=False, null=False, on_delete=models.PROTECT)
+    product_group = models.ForeignKey('ProductGroup',
+                                      blank=False, null=False, on_delete=models.PROTECT)
 
     def __str__(self):
         return self.title
-
