fd3c8629e04c377f2b7afed2458a39cdf33cd635
MalC-U <mwcalow@gmail.com>
2020-07-19 18:40:52 +0100

    Added model and get_context_data function to class displaying all photos

--- products/views.py
+++ products/views.py
@@ -1,14 +1,19 @@
 """Products app views that will be imported to project urls"""
 
 from django.views.generic import ListView, DetailView
-from .models import Product
+from .models import Product, ProductGroup
 
 class Products(ListView):
     """"Class that will be used as_view() and will display all products/photos"""
+    model = Product
     template_name = "photos.html"
-    context_object_name = "products"
     queryset = Product.objects.order_by('-title')
     paginate_by = 6
+
+    def get_context_data(self, **kwargs):
+        context = super(Products, self).get_context_data(**kwargs)
+        context['group'] = "Gallery"
+        return context
 
 class ProductDetails(DetailView):
     """Class that will be used as_view() and will display detais
