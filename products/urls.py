bb0581cf631e52bc9aea90ca21d064cd5849be98
MalC-U <mwcalow@gmail.com>
2020-07-19 18:57:08 +0100

    Created path to ProdustForests view in urlpatterns

--- products/urls.py
+++ products/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path('', views.Products.as_view(), name='all_products'),
     path('lakes/', views.ProductsLakes.as_view(), name='lakes_seas'),
     path('hills/', views.ProductsHills.as_view(), name='hills'),
+    path('forests/', views.ProductsForests.as_view(), name='forests'),
     path('<int:pk>/', views.ProductDetails.as_view(), name="product-detail"),
+    
 ]
