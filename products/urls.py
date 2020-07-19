a0ca99488c3eefc12c27c74d1575f71996987e86
MalC-U <mwcalow@gmail.com>
2020-07-19 18:45:19 +0100

    Amended import from products.views to . and how it is called in urlpatterns

--- products/urls.py
+++ products/urls.py
@@ -1,9 +1,9 @@
 """Products app urls to be imported to main project urls"""
 
 from django.urls import path
-from products.views import Products, ProductDetails
+from . import views
 
 urlpatterns = [
-    path('', Products.as_view(), name='all_products'),
-     path('photo/<int:pk>/', ProductDetails.as_view(), name="product-detail"),
+    path('', views.Products.as_view(), name='all_products'),
+    path('<int:pk>/', views.ProductDetails.as_view(), name="product-detail"),   
 ]
