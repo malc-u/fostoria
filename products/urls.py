6a3f94c5df9b8a59963eef13ff2ba08551c2604a
MalC-U <mwcalow@gmail.com>
2020-07-19 18:52:48 +0100

    Created path to ProductHills ListView in urlpatterns

--- products/urls.py
+++ products/urls.py
@@ -6,5 +6,6 @@ from . import views
 urlpatterns = [
     path('', views.Products.as_view(), name='all_products'),
     path('lakes/', views.ProductsLakes.as_view(), name='lakes_seas'),
+    path('hills/', views.ProductsHills.as_view(), name='hills'),
     path('<int:pk>/', views.ProductDetails.as_view(), name="product-detail"),
 ]
