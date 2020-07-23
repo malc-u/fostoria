0b167c80c79bad7e7303c2dfc7b8987448f63fe5
MalC-U <mwcalow@gmail.com>
2020-07-23 14:32:45 +0100

    Included cart urls in project urls

--- fostoria/urls.py
+++ fostoria/urls.py
@@ -29,4 +29,5 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
     path('photos/', include('products.urls')),
+    path('cart/', include('cart.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) # Included for ImageField to work
