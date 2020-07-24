431c52ad135daeafecc6a72a26efb7a330331852
MalC-U <mwcalow@gmail.com>
2020-07-24 18:58:24 +0100

    Created model that allows to set product sizes

--- products/models.py
+++ products/models.py
@@ -38,3 +38,10 @@ class PrintPrice(models.Model):
     print_price = models.DecimalField(max_digits=6, decimal_places=2)
     print_sizes = models.ManyToManyField('PrintSize', through='PricingSizes')
 
+
+class PrintSize(models.Model):
+    """Class that will allow admin set the prices for prints"""
+    print_size = models.CharField(max_length=120)
+    print_prices = models.ManyToManyField('PrintPrice', through='PricingSizes')
+
+
