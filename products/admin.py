c9dd00b3314f5ba73375f339a63ed65002640365
MalC-U <mwcalow@gmail.com>
2020-07-24 19:00:26 +0100

    Imported newly created models and registered them to admin.py

--- products/admin.py
+++ products/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from .models import ProductGroup, Product
+from .models import ProductGroup, Product, PrintPrice, PrintSize, PricingSizes
 
 # Register your models here.
 admin.site.register(ProductGroup)
 admin.site.register(Product)
+admin.site.register(PrintPrice)
+admin.site.register(PrintSize)
+admin.site.register(PricingSizes)
