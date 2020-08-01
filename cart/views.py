dfa37f523cba63a43876c0baa11de799180e60c9
MalC-U <mwcalow@gmail.com>
2020-08-01 19:52:10 +0100

    Added add_to_cart view that is checking if product_size posted in add to cart form

--- cart/views.py
+++ cart/views.py
@@ -1,8 +1,26 @@
 """Cart app views that will be imported to cart urls to be displayed"""
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+
 def cart_view(request):
     """A view that will render cart contents"""
 
     return render(request, "cart.html")
+
+def add_to_cart(request, article_id):
+    """ Add a quantity of the specified product to the shopping cart
+    with the size taken into account. This function was shared by Chris Zielinsky
+    during Code Institute's course Ado-Boutiqe module. It was amended by myself to suit
+    my project needs. """
+
+    qty = int(request.POST.get('qty'))
+    redirect_url = request.POST.get('redirect_url')
+
+    if 'product_size' in request.POST:
+        size = request.POST['product_size']
+    cart = request.session.get('cart', {})
+
+
+
+    request.session['cart'] = cart
+    return redirect(redirect_url)
