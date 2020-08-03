2d80fe4233d325a7a2924cc0273b127640ee8175
MalC-U <mwcalow@gmail.com>
2020-08-03 17:52:26 +0100

    Created path to about page and added missing top docstring

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,8 @@
+""" Home app urls to be imported to main project urls """
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
+    path('about/', views.about, name='about'),
 ]
