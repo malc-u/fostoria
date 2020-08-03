8b33914aa6fa04703e3ff993fe3ad575866b58bf
MalC-U <mwcalow@gmail.com>
2020-08-03 12:23:27 +0100

    Added function that will utilise update-form function to delete items from cart on reset btn click

---
+++ static/js/custom.js
@@ -0,0 +1,17 @@
+/**
+ * Remove item from cart by setting qty value to 0 in update form and submitting it 
+ * */ 
+
+$('.remove-from-cart').click(function(e) {
+        let articleId = $(this).attr('id');
+        let size = $(this).data('size');
+        let qty = 0;
+
+        $(".update-form-field").attr('data-article_is', 'articleId')
+            .attr('size', size)
+            .attr('name', 'qty')
+            .attr('value', qty)
+            .appendTo('#my-form');
+        $('#update-form').submit();
+    
+})
