f25fad0a30d17f2555257ec0aadc80c9e5450b60
MalC-U <mwcalow@gmail.com>
2020-08-03 14:08:25 +0100

    Created function increasing number of items in the cart on arrow up click. Qty will never go above above max as it returns to 1

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,4 +1,4 @@
-/**
+/*
  * Remove item from cart by setting qty value to 0 in update form and submitting it 
  * */ 
 
@@ -13,5 +13,24 @@ $('.remove-from-cart').click(function(e) {
             .attr('value', qty)
             .appendTo('#my-form');
         $('#update-form').submit();
+
     
 })
+
+/*
+ * Increasing qty of an item added to the cart
+ * */ 
+
+$('.increase-qty').click(function(e) {
+    e.preventDefault();
+    var qtyField = $(this).closest('#qty-group').find('.update-form-field')[0];
+    var currentQty = parseInt($(qtyField).val());
+    
+    if (currentQty < 10){
+        currentQty ++;
+    } else if ( currentQty = 10 ){
+        currentQty = 1;
+    }
+    $(qtyField).val(currentQty);
+        
+ });
