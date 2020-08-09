3ccce294e486a244c41b381c7e799a944abfbf85
MalC-U <mwcalow@gmail.com>
2020-08-09 19:01:41 +0100

    imported profile_view to accounts urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,7 @@
 """Accounts app urls that are imported to main project urls"""
 from django.contrib.auth import views as auth
 from django.urls import path
-from .views import register_view, logout
+from .views import register_view, logout, profile_view
 
 urlpatterns = [
     path('register/', register_view, name="register"),
