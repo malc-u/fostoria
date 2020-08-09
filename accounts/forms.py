f10017c1352f00118fde786202a5d1d3fcbc9acb
MalC-U <mwcalow@gmail.com>
2020-08-09 19:02:48 +0100

    Created UpdateUserForm that will allow user to update details - name and email

--- accounts/forms.py
+++ accounts/forms.py
@@ -15,3 +15,13 @@ class UserRegistrationForm(UserCreationForm):
         model = User
         fields = ['username', 'email', 'password1', 'password2']
 
+class UserUpdateForm(forms.ModelForm):
+    """
+    Form allowing user to update details.
+    """
+    email = forms.EmailField()
+
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
