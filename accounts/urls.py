37b112a6e7061fdaebb6df375a81c137afc43af9
MalC-U <mwcalow@gmail.com>
2020-08-09 17:21:12 +0100

    Created url for login registration utilising LoginView from django.contrib.auth

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,11 @@
 """Accounts app urls that are imported to main project urls"""
+from django.contrib.auth import views as auth
 from django.urls import path
 from .views import register_view
 
 urlpatterns = [
     path('register/', register_view, name="register"),
+    path('login/', auth.LoginView.as_view(template_name='login.html',
+                                          redirect_authenticated_user=True),
+         name="login"),
 ]
