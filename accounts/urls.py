e4f627f5dbed3105c742dc8c2d842873469daf7a
MalC-U <mwcalow@gmail.com>
2020-08-09 18:32:12 +0100

    changed path for logout to display popup message instead of redirecting to logout template

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,13 +1,12 @@
 """Accounts app urls that are imported to main project urls"""
 from django.contrib.auth import views as auth
 from django.urls import path
-from .views import register_view
+from .views import register_view, logout
 
 urlpatterns = [
     path('register/', register_view, name="register"),
     path('login/', auth.LoginView.as_view(template_name='login.html',
                                           redirect_authenticated_user=True),
          name="login"),
-    path('logout/', auth.LogoutView.as_view(template_name='logout.html'),
-         name="logout"),
+    path('logout/', logout, name="logout"),
 ]
