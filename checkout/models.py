beee9a51aaceebefc70a1e7c0477907382566e21
MalC-U <mwcalow@gmail.com>
2020-08-10 13:59:18 +0100

    Created Order shipping details that hold customer, address, phone number, date of order

--- checkout/models.py
+++ checkout/models.py
@@ -1,3 +1,23 @@
+"""Models of checkout app"""
+import datetime
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class OrderShippingDetails(models.Model):
+    """
+    Model storing all shipping details related to the order - customer, address, phone etc.
+    """
+    customer = models.ForeignKey(User, on_delete=models.PROTECT)
+    full_name = models.CharField(max_length=100, blank=False)
+    first_address_line = models.CharField(max_length=75, blank=False)
+    second_address_line = models.CharField(max_length=75, blank=True, null=True)
+    town_or_city = models.CharField(max_length=50, blank=False)
+    county = models.CharField(max_length=50, blank=True, null=True)
+    postcode = models.CharField(max_length=10, null=True)
+    country = models.CharField(max_length=50, blank=False)
+    phone_number = models.CharField(max_length=20, blank=False)
+    date_ordered = models.DateField(default=datetime.date.today, null=True)
+
+    def __str__(self):
+        return f"{self.id}-{self.customer}-{self.date_ordered}"
