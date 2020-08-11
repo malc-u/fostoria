da0860c6d348287e98cf54e2e5d5d08638cc5d15
MalC-U <mwcalow@gmail.com>
2020-08-11 15:31:00 +0100

    Created PaymentForm

--- checkout/forms.py
+++ checkout/forms.py
@@ -9,10 +9,49 @@ class OrderShippingForm(forms.ModelForm):
     class Meta:
         model = OrderShippingDetails
         fields = (
-            'full_name', 'first_address_line', 'second_address_line', 'town_or_city',
-            'county', 'postcode', 'country', 'phone_number'
-        ),
+            'full_name',
+            'first_address_line',
+            'second_address_line',
+            'town_or_city',
+            'county',
+            'postcode',
+            'country',
+            'phone_number'
+        )
         labels = {
             'first_address_line': "First Line Of Address",
             'Second_address_line': "Second Line Of Address",
         }
+
+class PaymentForm(forms.Form):
+    """
+    Form that is rendered for user to process the card payment. It renders 
+    all required fields for card payment processing.
+
+    This form was presented during Code Institute course but it is modified by me
+    to display months from January until December (inclusive)
+    """
+    MONTH_CHOICES = [(i, i) for i in range(1, 13)]
+    YEAR_CHOICES = [(i, i) for i in range(2020, 2035)]
+
+    credit_card_number = forms.CharField(
+        label='Credit card number',
+        required=False
+    )
+    cvv = forms.CharField(
+        label='Security code (CVV)',
+        required=False
+    )
+    expiry_month = forms.ChoiceField(
+        label='Expiry Month',
+        choices=MONTH_CHOICES,
+        required=False
+    )
+    expiry_year = forms.ChoiceField(
+        label='Expiry Year',
+        choices=YEAR_CHOICES,
+        required=False
+    )
+    stripe_id = forms.CharField(
+        widget=forms.HiddenInput()
+    )
