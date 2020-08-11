0c7af6d03916415dffe1dccfa489ebbe87d92d0a
MalC-U <mwcalow@gmail.com>
2020-08-11 15:29:05 +0100

    removed date_ordered from shipping details in checkout model OrderShipppingDetails

--- checkout/models.py
+++ checkout/models.py
@@ -17,7 +17,6 @@ class OrderShippingDetails(models.Model):
     postcode = models.CharField(max_length=10, null=True)
     country = models.CharField(max_length=50, blank=False)
     phone_number = models.CharField(max_length=20, blank=False)
-    date_ordered = models.DateField(default=datetime.date.today, null=True)
 
     def __str__(self):
-        return f"{self.id}-{self.customer}-{self.date_ordered}"
+        return f"{self.id}-{self.customer}"
