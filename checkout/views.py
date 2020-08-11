af2b950dd4d038592c3aa3d9f18c8b77d5b28bd0
MalC-U <mwcalow@gmail.com>
2020-08-11 15:07:45 +0100

    Added OrderSHippingForm to the view - if method ==POST then request.POSt if not then empty form

--- checkout/views.py
+++ checkout/views.py
@@ -1,9 +1,14 @@
+"""View of checkout application"""
 from django.shortcuts import render
-
+from .forms import OrderShippingForm
 
 # Create your views here.
 
 def checkout_delivery_view(request):
 
-    return render(request, template, context)
+    if request.method == 'POST':
+        order_shipping_form = OrderShippingForm(request.POST)
+    else:
+        order_shipping_form = OrderShippingForm()
 
+    return render(request, template, context)
