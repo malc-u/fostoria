b906a35ee6417ae2ae911f4c12cacdfc4e7fdea7
MalC-U <mwcalow@gmail.com>
2020-08-26 15:03:54 +0100

    added page title to the context in index view

--- home/views.py
+++ home/views.py
@@ -3,7 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     """ A view that is returning index page """
-    return render(request, 'index.html')
+    context = {
+        'title': 'Index Page'
+    }
+    return render(request, 'index.html', context)
 
 def about(request):
     """A vuew that returns about page"""
