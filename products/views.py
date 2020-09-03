2efed81401961cde8b5eabfa584ddf0bda7955ff
MalC-U <mwcalow@gmail.com>
2020-09-03 10:48:08 +0100

    Changed number of products to be displayed on each gallery category

--- products/views.py
+++ products/views.py
@@ -11,7 +11,7 @@ class Products(ListView):
     model = Product
     template_name = "photos.html"
     queryset = Product.objects.order_by('-title')
-    paginate_by = 12
+    paginate_by = 8
 
     def get_context_data(self, **kwargs):
         context = super(Products, self).get_context_data(**kwargs)
@@ -39,7 +39,7 @@ class ProductsLakes(ListView):
     model = Product
     template_name = "photos.html"
     queryset = Product.objects.filter(product_group__name='lakes_seas').order_by('title')
-    paginate_by = 12
+    paginate_by = 8
 
     def get_context_data(self, **kwargs):
         context = super(ProductsLakes, self).get_context_data(**kwargs)
@@ -53,7 +53,7 @@ class ProductsHills(ListView):
     model = Product
     template_name = "photos.html"
     queryset = Product.objects.filter(product_group__name='fields_hills').order_by('title')
-    paginate_by = 12
+    paginate_by = 8
 
     def get_context_data(self, **kwargs):
         context = super(ProductsHills, self).get_context_data(**kwargs)
@@ -67,7 +67,7 @@ class ProductsForests(ListView):
     model = Product
     template_name = "photos.html"
     queryset = Product.objects.filter(product_group__name='forests').order_by('-title')
-    paginate_by = 12
+    paginate_by = 8
 
     def get_context_data(self, **kwargs):
         context = super(ProductsForests, self).get_context_data(**kwargs)
