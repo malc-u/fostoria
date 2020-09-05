47977bf4283571642375172cc933d0903c014f6f
MalC-U <mwcalow@gmail.com>
2020-09-05 19:24:56 +0100

    Added import of get_object_or_404 to get product instance

--- checkout/views.py
+++ checkout/views.py
@@ -1,5 +1,5 @@
 """Views of the checkout app"""
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.conf import settings
 from django.contrib.auth.decorators import login_required
 import stripe
