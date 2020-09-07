217d89245d5ee825a4838c4458e516e3dbfe94b7
MalC-U <mwcalow@gmail.com>
2020-09-07 10:54:44 +0100

    Created about view in about app that will render about.html

--- about/views.py
+++ about/views.py
@@ -1,4 +1,11 @@
+"""About app views"""
 from django.shortcuts import render
 
 # Create your views here.
 
+def about(request):
+    """A view that returns about page"""
+    context = {
+        'title': 'About Page'
+    }
+    return render(request, 'about.html', context)
