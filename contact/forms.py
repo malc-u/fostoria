397596c79944644ca5df99fbc31aa69b3a6c4245
MalC-U <mwcalow@gmail.com>
2020-09-08 10:07:44 +0100

    Created contact form that will consist of 3 required fields email, message subject and message text

--- contact/forms.py
+++ contact/forms.py
@@ -1,3 +1,11 @@
 """Forms for Contact app """
 from django import forms
 
+class ContactForm(forms.Form):
+    
+    from_email = forms.EmailField(required=True)
+    subject = forms.CharField(required=True)
+    message = forms.CharField(required=True)
+
+    class Meta:
+        fields = ["subject", "from_email", "message"]
