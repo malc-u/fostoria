bef31d221d62693e2507276d95a95ce827940cf5
MalC-U <mwcalow@gmail.com>
2020-09-08 10:10:18 +0100

    Added extra atributes to the message field - label and placeholder

--- contact/forms.py
+++ contact/forms.py
@@ -8,7 +8,10 @@ class ContactForm(forms.Form):
     """
     from_email = forms.EmailField(required=True)
     subject = forms.CharField(required=True)
-    message = forms.CharField(required=True)
+    message = forms.CharField(widget=forms.Textarea(attrs={
+        'placeholder': "Enter Your Message Here"}),
+                              label="Message:",
+                              required=True)
 
     class Meta:
         fields = ["subject", "from_email", "message"]
