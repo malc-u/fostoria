ceba85008327094f1a45233182769feee08c0485
MalC-U <mwcalow@gmail.com>
2020-09-08 10:09:09 +0100

    Addd ContactForm docstring

--- contact/forms.py
+++ contact/forms.py
@@ -2,7 +2,10 @@
 from django import forms
 
 class ContactForm(forms.Form):
-    
+    """
+    Contact form that is rendered on contact.html and will
+    let customer to send message
+    """
     from_email = forms.EmailField(required=True)
     subject = forms.CharField(required=True)
     message = forms.CharField(required=True)
