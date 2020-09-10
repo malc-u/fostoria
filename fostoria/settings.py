8d15c86c7d819b03219327411686a0c53268cf41
MalC-U <mwcalow@gmail.com>
2020-09-10 22:44:34 +0100

    block possibility of overwriting s3 bucket files and set acl in aws default to none

--- fostoria/settings.py
+++ fostoria/settings.py
@@ -174,6 +174,8 @@ if 'HEROKU' in os.environ:
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+    AWS_S3_FILE_OVERWRITE = False
+    AWS_DEFAULT_ACL = None
 
     # Static and media files
     STATICFILES_STORAGE = 'custom_storages.StaticStorage'
