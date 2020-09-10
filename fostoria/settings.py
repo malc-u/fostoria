0d06f4b448e37fb1e7b4e4103225b0f6169af615
MalC-U <mwcalow@gmail.com>
2020-09-10 20:15:06 +0100

    Added settings required by Amazon Web Service

--- fostoria/settings.py
+++ fostoria/settings.py
@@ -174,3 +174,15 @@ EMAIL_HOST_USER = os.environ.get("EMAIL_HOST")
 EMAIL_HOST_PASSWORD = os.environ.get("HOST_PASS")
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
+
+# Amazon Web Service Required Settings
+AWS_S3_OBJECT_PARAMETERS = {
+    'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+    'CacheControl': 'max-age=99999999',
+}
+
+AWS_STORAGE_BUCKET_NAME = 'fostoria'
+AWS_S3_REGION_NAME = 'eu-west-1'
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
