2062d8b21178440d82c22ee757fec2745d13c15a
MalC-U <mwcalow@gmail.com>
2020-09-10 22:13:33 +0100

    Added extra AWS settings for S2 bucket preventing overwriting media files and allowing public acces to files

--- fostoria/settings.py
+++ fostoria/settings.py
@@ -167,6 +167,8 @@ AWS_S3_REGION_NAME = 'eu-west-1'
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_DEFAULT_ACL = None
+AWS_S3_FILE_OVERWRITE = False
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
@@ -188,8 +190,8 @@ else:
 #Files for project operation - location
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_URL = "/media/"
 
 # Sweetalert version for sweetify function.
 SWEETIFY_SWEETALERT_LIBRARY = "sweetalert2"
