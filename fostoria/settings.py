de2e6068c7849e9cb1bcb90abbd9e887958fbc1c
MalC-U <mwcalow@gmail.com>
2020-09-10 20:58:44 +0100

    turning Debug off once deployed

--- fostoria/settings.py
+++ fostoria/settings.py
@@ -27,7 +27,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+if os.getenv("HEROKU"):
+    DEBUG = False
+else:
+    DEBUG = True
 
 ALLOWED_HOSTS = ['fostoria.herokuapp.com', '127.0.0.1']
 
