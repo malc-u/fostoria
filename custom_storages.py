b81ebd93d3f59d30123c914eda5b1f2333d26f1e
MalC-U <mwcalow@gmail.com>
2020-09-10 20:37:54 +0100

    Setting S3 to store static files

---
+++ custom_storages.py
@@ -0,0 +1,13 @@
+ """Setting S3 to store static files""" 
+from django.conf import settings
+from storages.backends.s3boto3 import S3Boto3Storage
+
+
+class StaticStorage(S3Boto3Storage):
+    """Sets the static files location."""
+    location = settings.STATICFILES_LOCATION
+
+
+class MediaStorage(S3Boto3Storage):
+    """Sets the media files location."""
+    location = settings.MEDIAFILES_LOCATION
