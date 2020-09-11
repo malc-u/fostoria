739293f7d08ca7c21e3ab998382760284b4cd7a1
MalC-U <mwcalow@gmail.com>
2020-09-11 13:50:46 +0100

    listed all views imported in products app

--- products/urls.py
+++ products/urls.py
@@ -1,13 +1,13 @@
 """Products app urls to be imported to main project urls"""
 
 from django.urls import path
-from . import views
+from .views import Products, ProductsLakes, ProductsHills, ProductsForests, ProductDetails, product_search
 
 urlpatterns = [
-    path('', views.Products.as_view(), name='all_products'),
-    path('lakes/', views.ProductsLakes.as_view(), name='lakes_seas'),
-    path('hills/', views.ProductsHills.as_view(), name='hills'),
-    path('forests/', views.ProductsForests.as_view(), name='forests'),
-    path('<int:pk>/', views.ProductDetails.as_view(), name="product-detail"),
-    path('search/', views.product_search, name="product_search"),
+    path('', Products.as_view(), name='all_products'),
+    path('lakes/', ProductsLakes.as_view(), name='lakes_seas'),
+    path('hills/', ProductsHills.as_view(), name='hills'),
+    path('forests/', ProductsForests.as_view(), name='forests'),
+    path('<int:pk>/', ProductDetails.as_view(), name="product-detail"),
+    path('search/', product_search, name="product_search"),
     ]
