ea70021f8e835bfbabed29ee263c43dc660f96a1
MalC-U <mwcalow@gmail.com>
2020-09-11 16:36:30 +0100

    Removed commented out code re DATABASES

--- fostoria/settings.py
+++ fostoria/settings.py
@@ -113,8 +113,6 @@ WSGI_APPLICATION = 'fostoria.wsgi.application'
 if "DATABASE_URL" in os.environ and "HEROKU" in os.environ:
     DATABASES = {'default': dj_database_url.parse(
         os.environ.get('DATABASE_URL'))}
-"""   # https://devcenter.heroku.com/articles/heroku-postgresql#heroku-postgres-ssl
-    DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)"""
 else:
     DATABASES = {
         'default': {
