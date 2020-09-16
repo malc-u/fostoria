221f4bb44f3ba567a791f7df4234779a285f1a7e
MalC-U <mwcalow@gmail.com>
2020-09-16 17:35:36 +0100

    Created contact view tests

--- contact/tests.py
+++ contact/tests.py
@@ -1,3 +1,31 @@
-from django.test import TestCase
+"""Contact app tests"""
+from django.test import TestCase, Client
 
-# Create your tests here.
+# View test
+class TestContactView(TestCase, Client):
+    """
+    Testing "contact" view.
+    """
+
+    def setUp(self):
+        """
+        Create client to conduct unit tests.
+        """
+        self.client = Client()
+
+    def test_get_contact_page(self):
+        """
+        Test checking if view contact is sucessfully loaded (status 200)
+        when URL ("/contact/") called.
+        """
+        response = self.client.get("/contact/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_contact_template(self):
+        """
+        Test checking if correct template is used ('contact.html')
+        when URL ("/contact/") called.
+        """
+        response = self.client.get("/contact/")
+        self.assertTemplateUsed(response, 'contact.html')
+        self.assertTemplateNotUsed(response, 'products.html')
