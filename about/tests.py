6a7adf37c86b7f571e74f087023aa72c949b88ba
MalC-U <mwcalow@gmail.com>
2020-09-16 17:30:01 +0100

    Created tests for about app

--- about/tests.py
+++ about/tests.py
@@ -1,3 +1,31 @@
-from django.test import TestCase
+"""About app tests"""
+from django.test import TestCase, Client
 
 # Create your tests here.
+class TestAboutView(TestCase, Client):
+    """
+    Testing "about" view.
+    """
+    
+    def setUp(self):
+        """
+        Create client to conduct unit tests.
+        """
+        self.client = Client()
+
+    def test_get_about_page(self):
+        """
+        Test checking if view about is sucessfully loaded (status 200)
+        when URL ("/about/") called.
+        """
+        response = self.client.get("/about/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_about_template(self):
+        """
+        Test checking if correct template is used ('about.html')
+        when URL ("/about/") called.
+        """
+        response = self.client.get("/about/")
+        self.assertTemplateUsed(response, 'about.html')
+        self.assertTemplateNotUsed(response, 'contact.html')
