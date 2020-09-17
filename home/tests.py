c599aebb0e0667b519ff370c6183107074f66527
MalC-U <mwcalow@gmail.com>
2020-09-17 10:14:08 +0100

    Added template tests

--- home/tests.py
+++ home/tests.py
@@ -20,3 +20,23 @@ class TestIndexView(TestCase, Client):
         """
         response = self.client.get("/")
         self.assertEqual(response.status_code, 200)
+
+
+    def test_get_index_template(self):
+        """
+        Test checking if correct template is used ('index.html' and all
+        others that were included on this template) when URL ("/") called.
+        """
+        response = self.client.get("/")
+        self.assertTemplateUsed(response, 'index.html')
+        self.assertTemplateUsed(response, 'base.html')
+        self.assertTemplateUsed(response, 'includes/head.html')
+        self.assertTemplateUsed(response, 'includes/header.html')
+        self.assertTemplateUsed(response, 'includes/navbar-menu.html')
+        self.assertTemplateUsed(response, 'includes/nav-buttons-mobile.html')
+        self.assertTemplateUsed(response, 'includes/nav-buttons-desktop.html')
+        self.assertTemplateUsed(response, 'includes/top-footer-sec.html')
+        self.assertTemplateUsed(response, 'includes/footer.html')
+        self.assertTemplateUsed(response, 'includes/scripts.html')
+        self.assertTemplateNotUsed(response, 'contact.html')
+
