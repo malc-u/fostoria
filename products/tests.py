f12f0c0bd049e1d81a23339d112f27dcdc8a31da
MalC-U <mwcalow@gmail.com>
2020-09-17 17:21:44 +0100

    Created ProductGroup test

--- products/tests.py
+++ products/tests.py
@@ -1,3 +1,20 @@
+"""Products app tests"""
 from django.test import TestCase
+from .models import ProductGroup, ProductGroup
 
 # Create your tests here.
+class TestProductGroup(TestCase):
+    """
+    ProductGroup model test
+    """
+    def test_product_group_can_be_saved_with_one_field_only(self):
+        """
+        Testing that the model can be saved
+        with 1 field filled in and the other one remaining
+        empty string 
+        """
+        test_name = ProductGroup(name="Test name")
+        test_name.save()
+        self.assertEqual(test_name.name, "Test name")
+        self.assertEqual(test_name.display_name, "")
+
