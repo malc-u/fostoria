73c6c07caf4b3964f09df16023cbe00c0cc4d2dd
MalC-U <mwcalow@gmail.com>
2020-09-17 10:07:28 +0100

    Created tests for home app and checked whether view index is sucesfully loaded when url ("/") called

--- home/tests.py
+++ home/tests.py
@@ -1,3 +1,22 @@
-from django.test import TestCase
+"""Home app tests"""
+from django.test import TestCase, Client
 
 # Create your tests here.
+class TestIndexView(TestCase, Client):
+    """
+    Testing "index" view in a home app.
+    """
+
+    def setUp(self):
+        """
+        Create client to conduct unit tests.
+        """
+        self.client = Client()
+
+    def test_get_index_page(self):
+        """
+        Test checking if view index is sucessfully loaded (status 200)
+        when URL ("/") called.
+        """
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
