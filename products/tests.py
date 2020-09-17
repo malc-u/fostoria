f130ba7fe4a78242458ddf7971b01d445a49ae45
MalC-U <mwcalow@gmail.com>
2020-09-17 17:34:19 +0100

    Added test of Product model

--- products/tests.py
+++ products/tests.py
@@ -1,6 +1,6 @@
 """Products app tests"""
 from django.test import TestCase
-from .models import ProductGroup, ProductGroup
+from .models import ProductGroup, Product
 
 # Create your tests here.
 class TestProductGroup(TestCase):
@@ -11,10 +11,18 @@ class TestProductGroup(TestCase):
         """
         Testing that the model can be saved
         with 1 field filled in and the other one remaining
-        empty string 
+        empty string
         """
         test_name = ProductGroup(name="Test name")
         test_name.save()
         self.assertEqual(test_name.name, "Test name")
         self.assertEqual(test_name.display_name, "")
 
+class TestProduct(TestCase):
+    """
+    Product model test
+    """
+    def test_product_name_returned(self):
+        """Testing that correct product title is returned."""
+        test_product = Product(title="Test product")
+        self.assertEqual(test_product.title, "Test product")
