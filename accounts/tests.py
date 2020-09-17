b6e815fc9f99c88bd2f68471b488b8f8e635171e
MalC-U <mwcalow@gmail.com>
2020-09-17 11:31:35 +0100

    created register template test

--- accounts/tests.py
+++ accounts/tests.py
@@ -21,4 +21,20 @@ class TestRegisterView(TestCase, Client):
         response = self.client.get("/accounts/register/")
         self.assertEqual(response.status_code, 200)
     
-
+    def test_get_register_template(self):
+        """
+        Test checking if correct template is used ('register.html' and all
+        others that were included on this template) when URL ("/accounts/register/") called.
+        """
+        response = self.client.get("/accounts/register/")
+        self.assertTemplateUsed(response, 'register.html')
+        self.assertTemplateUsed(response, 'base.html')
+        self.assertTemplateUsed(response, 'includes/head.html')
+        self.assertTemplateUsed(response, 'includes/header.html')
+        self.assertTemplateUsed(response, 'includes/navbar-menu.html')
+        self.assertTemplateUsed(response, 'includes/nav-buttons-mobile.html')
+        self.assertTemplateUsed(response, 'includes/nav-buttons-desktop.html')
+        self.assertTemplateUsed(response, 'includes/top-footer-sec.html')
+        self.assertTemplateUsed(response, 'includes/footer.html')
+        self.assertTemplateUsed(response, 'includes/scripts.html')
+        self.assertTemplateNotUsed(response, 'contact.html')
