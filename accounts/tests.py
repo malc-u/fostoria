991aa72b02ddcdc8649a2de1ae0bbd97ca360cbd
MalC-U <mwcalow@gmail.com>
2020-09-17 11:31:14 +0100

    Created register_view response test

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,24 @@
-from django.test import TestCase
+"""Accounts app tests"""
+from django.test import TestCase, Client
 
 # Create your tests here.
+class TestRegisterView(TestCase, Client):
+    """
+    Testing register_view
+    """
+
+    def setUp(self):
+        """
+        Create client to conduct unit tests.
+        """
+        self.client = Client()
+
+    def test_get_register_page(self):
+        """
+        Test checking if register_view is sucessfully loaded (status 200)
+        when URL ("/accounts/register/") called.
+        """
+        response = self.client.get("/accounts/register/")
+        self.assertEqual(response.status_code, 200)
+    
+
