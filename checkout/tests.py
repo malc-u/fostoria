f599d66703cf33757de74498ee47545c18127920
MalC-U <mwcalow@gmail.com>
2020-09-20 08:01:52 +0100

    Test for PaymentForm limiting number of digits on cvv field to 3

--- checkout/tests.py
+++ checkout/tests.py
@@ -1,3 +1,15 @@
+""" Checkout app tests"""
 from django.test import TestCase
+from .forms import PaymentForm
 
 # Create your tests here.
+class TestPaymentForm(TestCase):
+    """
+    Testing Payment Form
+    """
+    def test_cannot_use_cvv_longer_than_three_digits(self):
+        """
+        Confirming that more than 1 field (username in this case) is needed to create new user
+        """
+        form = PaymentForm({'cvv': '1234'})
+        self.assertFalse(form.is_valid())
